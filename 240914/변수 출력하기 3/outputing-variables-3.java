c8df7fa66c8a52d19869bacd721a904abbca573d
Khan / 이창민 <EeeasyCode@gmail.com>
2024-09-14 03:22:47 +0900

    Title: 변수 출력하기 3, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240914/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        String c = "C";
+
+        System.out.print(a + "->" + b + "->" + c);
+    }
+}
