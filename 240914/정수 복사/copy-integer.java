4393e677a1faf908634b557c33550400af723fbd
Khan / 이창민 <EeeasyCode@gmail.com>
2024-09-14 03:42:19 +0900

    Title: 정수 복사, Time: 103ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240914/정수 복사/copy-integer.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3, b = 4;
+        b = a;
+        System.out.print(a + " " + b);
+        System.out.print(a*b);
+    }
+}
