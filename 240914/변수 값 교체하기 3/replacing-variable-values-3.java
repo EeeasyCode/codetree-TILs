e3c7d5c38e71836d095205286ea5bb774ea16e70
Khan / 이창민 <EeeasyCode@gmail.com>
2024-09-14 03:43:40 +0900

    Title: 변수 값 교체하기 3, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240914/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3, b = 5;
+        int temp;
+
+        temp = b;
+        b = a;
+        a = temp;
+
+        System.out.print(a + "\n");
+        System.out.print(b);
+    }
+}
