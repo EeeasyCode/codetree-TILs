ab4d1cdd6d0a23a6bc69ffaead9ce18236d8f20d
Khan / 이창민 <EeeasyCode@gmail.com>
2024-09-23 00:33:16 +0900

    Title: 정수 명령 처리 5, Time: 472ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/정수 명령 처리 5/process-numeric-commands-5.java
+++ 240923/정수 명령 처리 5/process-numeric-commands-5.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
@@ -6,26 +9,26 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i=0; i<n; i++) {
-            int oper = sc.next();
+            String oper = sc.next();
 
-            if (oper == "size") {
+            if (oper.equals("size")) {
                 System.out.println(arr.size());
+                continue;
+            }
+            
+            if (oper.equals("pop_back")) {
+                arr.remove(arr.size()-1);
                 continue;
             }
 
             int num = sc.nextInt();
 
-            if (oper == "push_back") {
+            if (oper.equals("push_back")) {
                 arr.add(num);
                 continue;
             }
 
-            if (oper == "pop_back") {
-                arr.remove(arr.size()-1);
-                continue;
-            }
-
-            if (oper == "get") {
+            if (oper.equals("get")) {
                 System.out.println(arr.get(num-1));
                 continue;
             }
