ae693e8958bb52880f5b1312d27a971905298106
Khan / 이창민 <EeeasyCode@gmail.com>
2025-01-03 20:53:41 +0900

    Title: 정수 직사각형, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250103/정수 직사각형/rectangle-of-numbers.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+num_arr = [[0] * m for _ in range(n)]
+
+num = 1
+for row in range(n):
+    for col in range(m):
+        num_arr[row][col] = num
+        num += 1
+
+print(num_arr)
